a345b34ccd143edc9067fae47fe63b27ed2271f5
dengxin_dev <dengxin590@foxmail.com>
2020-12-15 14:51:44 +0800

    git init

--- src/main/java/com/mycode/javalearn/JavaLearnApplication.java
+++ src/main/java/com/mycode/javalearn/JavaLearnApplication.java
@@ -3,6 +3,9 @@ package com.mycode.javalearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author dengxin
+ */
 @SpringBootApplication
 public class JavaLearnApplication {
 
