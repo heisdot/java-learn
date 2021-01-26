8c65ce0de17c56a2e8e96335749055043808a2ba
heisdot <1424511182@qq.com>
2021-01-26 21:40:42 +0800

    实体类使用lombok方法

--- src/main/java/com/mycode/javalearn/model/DO/User.java
+++ src/main/java/com/mycode/javalearn/model/DO/User.java
@@ -1,13 +1,21 @@
 package com.mycode.javalearn.model.DO;
 
-import lombok.Data;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @Description TODO
  * @Author toolman
  * @Date 2020/12/15 21:53
  **/
-@Data
+//@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Slf4j
 public class User {
     private String id;
 
